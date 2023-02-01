201539ecd5a2b3d9aa0202fa9ec06979610f4413
laegel <laegel@selper.co.kr>
2023-02-01 18:15:31 +0900

    Title : 주문 로직 테스트

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OrderServiceTest {
+
+}
